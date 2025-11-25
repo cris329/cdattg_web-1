41cc5c4ed27ac2d72722931bee781600ed623afc
Deyson Urrego <deysonurrego159@gmail.com>
2025-11-25 11:10:42 -0500

    Tipo de retorno en el Traits seguimiento

--- app/Traits/Seguimiento.php
+++ app/Traits/Seguimiento.php
@@ -3,31 +3,31 @@
 namespace App\Traits;
 
 use App\Models\User;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 trait Seguimiento
 {
     
     // Relación con el usuario que creó el registro
-    public function userCreate()
+    public function userCreate() : BelongsTo
     {
         return $this->belongsTo(User::class, 'user_create_id');
     }
     
     // Relación con el usuario que actualizó el registro por última vez
-    public function userUpdate()
+    public function userUpdate() : BelongsTo
     {
         return $this->belongsTo(User::class, 'user_update_id');
     }
     
     // Alias de userCreate() para compatibilidad
-    public function creador()
+    public function creador() : BelongsTo
     {
         return $this->userCreate();
     }
 
     // Alias de userUpdate() para compatibilidad
-    public function actualizador()
+    public function actualizador() : BelongsTo
     {
         return $this->userUpdate();
     }
